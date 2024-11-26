c8e3ee7387b77339cb2634240a229c1502fa36f3
EmmGautier <emmanuel.gautier@free.fr>
2024-11-26 18:25:01 +0100

    Évolution streamlit

--- streamlit/st_accidentologie.py
+++ streamlit/st_accidentologie.py
@@ -1,7 +1,7 @@
 import streamlit as st
-
+import json
 import pandas as pd
-import numpy as np
+#import numpy as np
 
 from sklearn.decomposition         import PCA
 from sklearn.preprocessing         import StandardScaler
@@ -14,83 +14,96 @@ import matplotlib.pyplot as plt
 
 import joblib
 
+rep_raw = "data/raw/"
+rep_processed = "data/processed/"
+rep_models = "models/"
+rep_figures = "reports/figures/"
+rep_ref = "references/"
+
 st.sidebar.title("Sommaire")
 
 pages=["Présentation", "Data Vizualization", "Modélisation pas SVC", "Modélisation par Deep Learning"]
 page=st.sidebar.radio("Aller vers", pages)
 st.sidebar.write("Erika Méronville\n&\nEmmanuel Gautier")
 
-###############################################################################
+
+##############################################################################
 #
-#  Lecture des données.
+# Chargements en mémoire
 #
-#  Cette lecture est réalisée par la fonction read_data().
-#  Pour éviter la relecture à chaque commande elle est décorée par @st.cache_data
+# Ces chargements sont réalisés par des fonction décorées par @st.cache_data
+# pour que les lectures soient réalisées une seule fois.
 #
-###############################################################################
+##############################################################################
+
+# Lecture des modèles entraînés
 
 @st.cache_data
-def read_data():
+def read_models():
 
-    SVC, pca, X_zero, scaler, DP, scaler_DP = None, None, None, None, None, None
-
-    try : 
-        SVC = joblib.load("models/SVC.mdl")
-        pca = joblib.load("models/pca_ml.mdl")
-    except Exception as inst:
-        st.write (inst.args)
-        st.write (inst)
-        SVC, pca = None, None
+    SVC, pca, scaler_ml, scaler_DP = None, None, None, None
 
     try:
-        scaler_ml = joblib.load("models/acc_scaler.mdl")
+        SVC       = joblib.load(rep_models + "SVC.mdl")
+        pca       = joblib.load(rep_models + "pca_ml.mdl")
+        scaler_ml = joblib.load(rep_models + "acc_scaler.mdl")
+        scaler_DP = joblib.load(rep_models + "/scaler_DP.joblib")
 
-        scaler_DP = joblib.load("models/scaler_DP.joblib")
+    except Exception as e:
+        st.error(f"Erreur lors du chargement du modèle: {str(e)}")
 
-        # TODO activer après compilation de tensorflow
-        # DP = tf.saved_model.load("models/acc_DeepLearning.h5")
+    return SVC, pca, scaler_ml, scaler_DP
 
-    except Exception as inst:
-        st.write (inst.args)
-        st.write (inst)
-        DP = None
+# Lecture du jeu de données avec une observation à zero
+# ce jeu de données avec une seule observation est utilisé par la démo.
 
-    # Lectures des synthèses pour la visualisation de données
-    # Données synthétiques car plus rapide et faible espace de stockage
+@st.cache_data
+def read_df_zero():
+
+    df_zero = None
+
     try:
-        df_zero        = pd.read_csv("data/processed/data_zero.csv",      sep = '\t')
-        df_evol_grav   = pd.read_csv("data/processed/evol_grav.csv",      sep = '\t')
-        stats_expl_var = pd.read_csv("data/processed/stats_expl_var.csv", sep = '\t')
-        # st.write (f" --> df_zero 1 : {df_zero.shape[0]} x {df_zero.shape[1]}")
-        X_zero = df_zero.drop(["grav_grave", "agg"], axis = 1)
-        # st.write (f" --> df_zero 2 : {df_zero.shape[0]} x {df_zero.shape[1]}")
-        #X_zero = df_zero.drop("agg",        axis = 1)
+        df_zero = pd.read_csv(rep_processed + "data_zero.csv", sep='\t')
 
-    except Exception as inst:
-        st.write (inst.args)
-        st.write (inst)
-        SVC, pca = None, None
-        pass
+    except Exception as e:
+        st.error(f"Erreur lors du chargement du jeu de données vide : {str(e)}")
 
-    # La fonction est "décorée" avec @st.cache_data pour éviter les relectures
-    # à chaque affichage d'une page. Les résultats renvoyés avec return sont
-    # alors mis en cache. les resultats sont donc mis dans un dictionnaire.
-    return {"SVC" : SVC, "PCA" : pca, "X_zero" : X_zero,
-            "scaler_ml" : scaler_ml, "DP" : DP,
-            "evol_grav" : df_evol_grav,
-            "scaler_DP" : scaler_DP,
-            "stats_expl_var" : stats_expl_var}
+    return df_zero
 
-llll = read_data()
-SVC = llll["SVC"]
-DP = llll["DP"]
-pca = llll["PCA"]
-X_zero = llll["X_zero"]
-scaler_ml = llll["scaler_ml"]
-scaler_DP = llll["scaler_DP"]
-df_evol_grav = llll["evol_grav"]
-stats_expl_var = llll["stats_expl_var"]
+# Lecture du jeu de données avec une observation à zero
 
+@st.cache_data
+def read_stats():
+
+    df_evol_grav, stats_expl_var = None, None
+
+    try:
+        df_evol_grav   = pd.read_csv(rep_processed + "evol_grav.csv",      sep='\t')
+        stats_expl_var = pd.read_csv(rep_processed + "stats_expl_var.csv", sep='\t')
+
+    except Exception as e:
+        st.error(f"Erreur lors du chargement du jeu de données vide : {str(e)}")
+
+    return df_evol_grav, stats_expl_var
+
+@st.cache_data
+def read_info():
+
+    desc_fic_raw, desc_vars = None, None
+
+    try:
+        with open(rep_ref + "desc_fic_raw.json", 'r', encoding='utf-8') as fichier:
+            desc_fic_raw = json.load(fichier)
+
+        with open(rep_ref + "desc_vars.json", 'r', encoding='utf-8') as fichier:
+            desc_vars = json.load(fichier)
+
+    except Exception as e:
+        st.error(f"Erreur lors du chargement du fichier d'information : {str(e)}")
+
+    return desc_fic_raw, desc_vars
+
+desc_fic_raw, desc_vars = read_info()
 
 ##############################################################################
 #
@@ -142,28 +155,15 @@ la gravité des accidents routiers en fonction des circonstances qui les entoure
 #
 ##############################################################################
 
-if page == pages[1] :
+if page == pages[1]:
+
+    df_evol_grav, stats_expl_var = read_stats()
+
     st.header("Visualisation du jeu de données")
 
-    st.subheader ("Évolution sur 19 ans des nombres d'usagers impliqués")
+    st.subheader("Évolution sur 19 ans des nombres d'usagers impliqués")
 
-    annee = df_evol_grav["annee"].max()
-    annee = st.selectbox("Année : ", df_evol_grav["annee"])
-
-    mod_grav_nb = df_evol_grav[df_evol_grav["annee"] == annee][["grav_1", "grav_2", "grav_3", "grav_4"]]
-
-    fig = plt.figure(figsize=(10, 6))
-    plt.title("Répartition de la gravité")
-
-    mod_grav_nb = mod_grav_nb.rename(columns = {"grav_1" : "Indemne", "grav_2" : "Tués",
-                         "grav_3" : "Blessés hospitalisés", "grav_4" : "Blessés legers"})
-    sns.barplot (mod_grav_nb)
-    #plt.ylim(0, df_evol_grav[["grav_1", "grav_2", "grav_3", "grav_4"]].max())
-    plt.ylim(0, df_evol_grav["grav_1"].max())
-
-    st.pyplot(fig)
-
-    df_base100 = df_evol_grav 
+    df_base100 = df_evol_grav
     df_base100["grav_1"] = 100. * df_base100["grav_1"] / df_base100.loc[0, "grav_1"]
     df_base100["grav_2"] = 100. * df_base100["grav_2"] / df_base100.loc[0, "grav_2"]
     df_base100["grav_3"] = 100. * df_base100["grav_3"] / df_base100.loc[0, "grav_3"]
@@ -182,7 +182,7 @@ if page == pages[1] :
     plt.legend(title='Modalités', loc = "upper center")
     plt.axvline(x=2019, color='black', linestyle=(0, (5, 5)), linewidth=2)
     plt.annotate("Changement\nde codification\nde notre var. cible", xy=(2019, 90),
-                  xytext = (2019.5, 95),
+                 xytext = (2019.5, 95),
                  arrowprops=dict(facecolor='black', arrowstyle='->', lw=1.5))
 
     plt.annotate("Effet COVID",
@@ -192,22 +192,49 @@ if page == pages[1] :
 
     plt.grid()
     st.pyplot(fig)
+    annee = df_evol_grav["annee"].max()
+    annee = st.selectbox("Année : ", df_evol_grav["annee"])
 
-    st.subheader ("Répartitions par modalités")
+    mod_grav_nb = df_evol_grav[df_evol_grav["annee"] == annee][["grav_1", "grav_2", "grav_3", "grav_4"]]
+
+    fig = plt.figure(figsize=(10, 6))
+    plt.title("Répartition de la gravité")
+
+    mod_grav_nb = mod_grav_nb.rename(columns = {"grav_1" : "Indemne", "grav_2" : "Tués",
+                         "grav_3" : "Blessés hospitalisés", "grav_4" : "Blessés legers"})
+    sns.barplot (mod_grav_nb)
+    #plt.ylim(0, df_evol_grav[["grav_1", "grav_2", "grav_3", "grav_4"]].max())
+    plt.ylim(0, df_evol_grav["grav_1"].max())
+
+    st.pyplot(fig)
+
+    #
+    # Affichage de répartitions de modalités par variables
+    #
+
+    st.subheader("Répartitions par modalités")
 
     # TODO : trier la liste des rubriques par ordre alphabétique
     rubriques = stats_expl_var["rubrique"].unique()
-    rubrique = st.selectbox("Rubrique : ", rubriques)
-    variables = stats_expl_var[stats_expl_var["rubrique"] == rubrique]["variable"].unique()
-    variable = st.selectbox("Variable : ", variables)
+    chx_rubrique = st.selectbox("Rubrique : ", rubriques)
+    variables = stats_expl_var[stats_expl_var["rubrique"] == chx_rubrique]["variable"].unique()
+    variables_lib = [] # Liste des variables avec les libellés explicites
+    for v in variables:
+        if desc_vars[v].get("label") is not None:
+            variables_lib.append(v + " : " + desc_vars[v].get("label"))
+        else:
+            variables_lib.append(v + " : ")
+    chx_variable = st.selectbox("Variable : ", variables_lib)
+    variable_df   = chx_variable.split(" : ")[0]
+    variable_expl = chx_variable.split(" : ")[1]
 
-    df1 = stats_expl_var[(stats_expl_var["rubrique"] == rubrique) & (stats_expl_var["variable"] == variable)]
+    df1 = stats_expl_var[(stats_expl_var["rubrique"] == chx_rubrique) & (stats_expl_var["variable"] == variable_df)]
     df1["count"] = df1["count"].astype("int")
     df1 = df1[["modalite", "count"]]
     # st.dataframe (df1) # pour mise au point, à supprimer
 
     fig = plt.figure(figsize=(10, 6))
-    plt.title(f"Répartition selon {variable}")
+    plt.title(f"Répartition selon {chx_variable}")
 
     sns.barplot (df1, y = "modalite", x= "count", orient = "h")
 
@@ -220,11 +247,13 @@ if page == pages[1] :
 ##############################################################################
 
 if page == pages[2] :
+
+    X_zero = read_df_zero()
+
     st.header("Modélisation par SVC")
     st.subheader("Performances du modèle")
 
     Xp = X_zero.copy()
-
 
     choix = ["Conducteur", "Passager", "Piéton"]
     catu = st.selectbox("Catégorie d'usager", choix)
@@ -262,42 +291,109 @@ if page == pages[2] :
 #
 # Modélisation : Deep Learning
 #
+# Cette page présente :
+#  - des choix de circonstances;
+#  - ? ? ? le choix du modèle  ? ? ?;
+#  - Les valeurs données au modèle ;
+#  - la prédiction de la gravité avec le ou les modèles;
+#  - la probalité de la gravité
+#
 ##############################################################################
 
 if page == pages[3] :
     st.write("### Modélisation par Deep Learning")
 
+    SVC, pca, scaler_ml, scaler_DP = read_models()
+
+    X_zero = read_df_zero()
+    X_zero = X_zero.drop("grav_grave", axis = 1)
+    #X_zero = X_zero.drop("agg", axis = 1)
     Xp = X_zero.copy()
 
-    #----- Choix catu : Catégorie d'usager : conducteur, passager ou piéton -----
+    choix = {} # Liste des choix de modalités ou circonstances
+    choix_inv = {} # Dictionnaires qui permet de retrouver  les noms de variables
+                   # à partir des libellés retournés par les st.selectbox()
+                   # Cela sera plus rapide que de rechercher les clefs à partir de valeurs
 
-    choix = ["Conducteur", "Passager", "Piéton"]
-    catu = st.selectbox("Catégorie d'usager", choix)
-    st.write(f"Catégorie d'usager {catu}")
-    if catu == choix[0] : 
-        Xp["catu_1"]  = 1
-    elif catu== choix[1] : 
-        Xp["catu_2"]  = 1
-    else:
-        Xp["catu_3"]  = 1
+    # La liste des choix des variables est dans le dictionnaire lst_var_pred
+    # Ce dictionnaire est complété, avec les informations de desc_var.json
+    #  L'ajout ou la suppression de variables se fait en modifiant la liste suivante
+    lst_var_pred = {"secu" : {}, "choc" : {}, "obs" : {}, "obsm" : {}, "catv" : {}, "trajet" : {}, "jour" : {}}
 
-    #----- Choix surf : État de la surface -----
-    choix = ["Normale", "Mouillée", "glissante", "autre"]
-    chps = ["surf_norm", "surf_mouil", "surf_gliss", "surf_autre"]
-    surf = st.selectbox("Catégorie d'usager", choix)
-    chp = chps[choix.index(surf)]
-    Xp[chp] = 1
-    st.write (f"choix : {surf} champ : {chp}")
+    for var in lst_var_pred.keys():
+        lst_var_pred[var]["modalites"] = {}
+        label_var = desc_vars[var].get("label")
+        lst_var_pred[var]["libelle"] = label_var
+        choix_inv[label_var] = {"var" : var, "mod" : {}}
+        for col in X_zero.columns:
+            if col.startswith(var + "_"):
+                #lst_var_pred[var]["modalites"].append(desc_vars.get(col).get("label"))
+                clef = col.split("_")[0]
+                modalite = col.split("_")[1]
+                #st.write(f" clef : {clef} modalité {modalite}\n")
+                label = desc_vars.get(col).get("label").split(" : ")[1]
+                code_label = desc_vars.get(col).get("name")
+                lst_var_pred[var]["modalites"][modalite] = label
+                choix_inv[label_var]["mod"][label] = code_label
 
+    # st.write(lst_var_pred) # Pour mise au point
+
+    # Création des choix de modalités
+    # à partir du dictionnaire des choix
+    for clefs, item in lst_var_pred.items():
+        libelle = item["libelle"]
+        #st.write(f"Choix : {libelle}\n") # Pour mise au point
+        #st.write(f"modalités : {item['modalites']}\n") # Pour mise au point
+        choix_mod = []
+        #choix_mod_clef = []
+        for mod, lib_mod in item["modalites"].items():
+            choix_mod.append(lib_mod)
+            #choix_mod_clef.append(mod)
+        choix[libelle] = st.selectbox(libelle, choix_mod, placeholder="Choisissez une option", index = None)
+
+    # Récupération des choix de l'utilisateur
+    # Les choix sont dans les menus de type st.selectbox() enregistrés dans choix
+    for lib, mod in choix.items():
+        #st.write(f"{lib} : {mod}")
+        #choix_inv[label_var]["mod"][label] = code_label
+        nom_var_sel = choix_inv[lib]['mod'].get(mod)
+
+        #st.write(f"   ----:: {choix_inv[lib]['mod'].get(mod)}")
+        #st.write(f"   ----> {nom_var_sel}")
+        if nom_var_sel is not None:
+            Xp[nom_var_sel] = 1
+
+        # lib et mod contiennent les libellés des variables et les libellés des modalités.
+        # il faut retrouver les noms d'origine des
+        #st.write(f" Modalité : {mod} {lib}")
+        #for
+        #desc_vars.valu
+
+    #st.write(f" ---->  {choix_inv}\n")
 
     st.dataframe(Xp)
 
-    Xp = scaler_DP.transform(Xp)
+    for col in Xp.columns:
+        # st.write (f" colonne : {col}")
+        if Xp.loc[0, col] != 0:
+            st.write (f"{col} : {desc_vars[col].get('label')}")
+
+
+    st.write(f"Mise à l'échelle")
+    Xp = scaler_ml.transform(Xp)
+    st.write(f"PCA")
+    Xp = pca.transform(Xp)
+    st.write(f"Prédiction")
+    pred = SVC.predict(Xp)
+
+    st.write(f"Prédiction : {pred}")
+
+    #Xp = scaler_DP.transform(Xp)
 
     st.write ("Développement en cours")
 
     # TODO Activer après compilation de tensorflow
-    # ypred = DP.predict(Xp)
+    #ypred = DP.predict(Xp)
 
     #st.write(f"Prédiction : {ypred}")
 
